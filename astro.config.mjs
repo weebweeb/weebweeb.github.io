1a1ca04027fd15710f0d3be20f9d8961435720c7
Alice <epicalice1234@gmail.com>
2026-02-11 22:10:49 -0600

    Update astro.config.mjs to define site URL and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://weebweeb.github.io',
+  // base: '/', // Do not set the base
+});
